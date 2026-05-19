25303ec06465bd2eb9c8b9368007db1a5cc7c21e
faridaseif <faridaahmedseif253@gmail.com>
2026-05-20 00:47:42 +0300

    Add files via upload

---
+++ backend/src/main/java/com/example/demo/dto/ApiResponse.java
@@ -0,0 +1,20 @@
+package com.example.demo.dto;
+
+public class ApiResponse {
+
+    private String message;
+    private Object data;
+
+    public ApiResponse(String message, Object data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+}
